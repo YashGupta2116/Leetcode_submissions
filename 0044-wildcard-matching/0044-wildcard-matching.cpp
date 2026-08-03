45af4c62cb0d825dcb205acbb1d9338153beb0fa
Yash Gupta <158163628+YashGupta2116@users.noreply.github.com>
2026-08-03 18:17:20 +0530

    Time: 55 ms (47.42%), Space: 15.7 MB (66.98%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
-    bool f(string& s, string& p, int i, int j, vector<vector<int>>& dp) {
-        if (i < 0 && j < 0) return true;
-        if (j < 0) return false;
-        if (i < 0) {
-            while (j >= 0) {
-                if (p[j] != '*') return false;
-                j--;
+    bool f(string& s, string& p, int n, int m) {
+        vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
+        dp[0][0] = true;
+        for (int i = 1; i <= n; i++) dp[i][0] = false;
+        for (int j = 1; j <= m; j++) {
+            bool flag = true;
+            for (int jj = 1; jj <= j; jj++) {
+                if (p[jj-1] != '*') {
+                    flag = false;
+                    break;
+                }
             }
-            return true;
+            dp[0][j] = flag;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i-1] == p[j-1] || p[j-1] == '?') {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if (p[j-1] == '*') {
+                    dp[i][j] = (dp[i][j-1] || dp[i-1][j]);
+                }
+                else dp[i][j] = false;
+            }
         }
 
-        if (dp[i][j] != -1) return dp[i][j];
-
-
-        if (s[i] == p[j] || p[j] == '?') 
-            return dp[i][j] = f(s, p, i-1, j-1, dp);
-        if (p[j] == '*')
-            return dp[i][j] = (f(s, p, i, j-1, dp) || f(s, p, i-1, j, dp));
-
-        return dp[i][j] = false;
+        
+        return dp[n][m];
     }
 
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.size(), vector<int>(p.size(), -1));
-        return f(s, p, s.size()-1, p.size()-1, dp);
+        return f(s, p, s.size(), p.size());
     }
 };
