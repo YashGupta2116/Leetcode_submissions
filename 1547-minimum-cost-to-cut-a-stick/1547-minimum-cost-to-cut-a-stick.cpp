c0f42929b24591d938019fe740bbb42c93f1c688
Yash Gupta <158163628+YashGupta2116@users.noreply.github.com>
2026-08-15 07:39:31 +0530

    Time: 15 ms (93.74%), Space: 13.9 MB (81.66%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int f(vector<int>& cuts, int i, int j, vector<vector<int>>& dp) {
-        if (i > j) return 0;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int res = INT_MAX;
-        for (int k = i; k <= j; k++) {
-            int cost = cuts[j+1] - cuts[i-1] + f(cuts, i, k-1, dp) + f(cuts, k+1, j, dp);
-            res = min(res, cost);
-        }
-
-        return dp[i][j] = res;
-    }
-
     int minCost(int n, vector<int>& cuts) {
         cuts.push_back(n);
         cuts.insert(cuts.begin(), 0);
         sort(cuts.begin(), cuts.end());
-        vector<vector<int>> dp(cuts.size(), vector<int>(cuts.size(), -1));
-        return f(cuts, 1, cuts.size() - 2, dp);
+        int m = cuts.size();
+        vector<vector<int>> dp(m, vector<int>(m, 0));
+
+
+        for (int i = m-2; i >= 1; i--) {
+            for (int j = i; j <= m-2; j++) {
+                int res = INT_MAX;
+                for (int k = i; k <= j; k++) {
+                    int cost = cuts[j+1] - cuts[i-1] + dp[i][k-1] + dp[k+1][j];
+                    res = min(res, cost);
+                }
+                dp[i][j] = res;
+            }
+        }
+        
+
+        return dp[1][m-2];
     }
 };
