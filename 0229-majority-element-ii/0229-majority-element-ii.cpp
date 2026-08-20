ad3b7dcd52174a8b4cd6cc9ed8313ed17c2139bd
Yash Gupta <158163628+YashGupta2116@users.noreply.github.com>
2026-08-20 18:43:37 +0530

    Time: 0 ms (100%), Space: 23.6 MB (79.33%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,17 +2,34 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> res;
-        unordered_map<int, int> mpp;
         int n = nums.size();
+        int cand1 = 0, cand2 = 1;
+        int count1 = 0, count2 = 0;
         for (int i = 0; i < n; i++) {
-            mpp[nums[i]]++;
-        }
-
-        for (const auto& it: mpp) {
-            if (it.second > n/3) {
-                res.push_back(it.first);
+            if (nums[i] == cand1) {
+                count1++;
+            } else if (nums[i] == cand2) {
+                count2++;
+            } else if (count1 == 0) {
+                cand1 = nums[i];
+                count1 = 1;
+            } else if (count2 == 0) {
+                cand2 = nums[i];
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
             }
         }
+
+        count1 = count2 = 0;
+        for (int& num: nums) {
+            if (num == cand1) count1++;
+            if (num == cand2) count2++;
+        }
+
+        if (count1 > n/3) res.push_back(cand1);
+        if (count2 > n/3) res.push_back(cand2);
 
         return res;
     }
